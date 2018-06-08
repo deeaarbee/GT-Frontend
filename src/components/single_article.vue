a27ad29e3e5ef71c965316bec030861b32d81677
saravanan2910 <saravanansmart13@gmail.com>
2018-06-08 21:40:44 +0530

    new form api

--- src/components/single_article.vue
+++ src/components/single_article.vue
@@ -1,19 +1,21 @@
 <template>
-    <div v-cloak>
+    <div class="home">
         <div class="loader">
         <pacman-loader :loading="loading" :color="color" :size="size" ></pacman-loader>
         </div>
             <div class="Single_Article" v-show =!loading>
-                <sidebar></sidebar>
+                <navbar class="navbar"></navbar>
+                <sidebar class="sidebar"></sidebar>
+
                 <div class="single_article-container v-cloak--hidden">
                     <div class="single-article" >
 
-                <div class="article-heading">
-                    <h2><strong>{{heading}}</strong></h2>
-                    <hr>
-                </div>
+                        <div class="article-heading">
+                            <h2><strong>{{heading}}</strong></h2>
+                            <hr>
+                        </div>
 
-                <div class="article-item">
+                        <div class="article-item">
                     <div class="heading">
                         <h1>
                             {{articles.title}}
@@ -56,8 +58,51 @@
 
                     <hr>
                 </div>
+                        <div class="article-item-mob">
+                            <div class="heading">
+                                <h1>
+                                    {{articles.title}}
+                                </h1>
+                            </div>
+                            <div class="authors">
+                                <ul>
+                                    <a v-bind:href="'/category/'+articles.genre"><li><strong>{{articles.genre}}</strong></li></a>
+                                    <li v-for="item in articles.author_list"><a v-bind:href="'/author/'+item"><strong>{{item}}</strong></a></li>
+                                    <li> {{articles.updated_at}}</li>
+                                </ul>
+                            </div>
 
-                <div class="related-articles">
+                            <!--<div class="image">-->
+                            <!--<img v-bind:src="'https://api.guindytimes.com/'+articles.image" alt="">-->
+                            <!--</div>-->
+                            <div class="content">
+
+                                <div v-html=articles.content class="article-content"></div>
+
+                            </div>
+                            <div class="tags-mob">
+                                <div class="tagged">
+                                    <p><strong>Tagged in :</strong></p>
+                                    <ul class="tag-list-mob">
+                                        <li v-for="item in articles.tags"><a v-bind:href="'/tag/'+item">{{item}}</a></li>
+                                    </ul>
+                                </div>
+
+                                <div class="share-mob">
+                                    <div class="tweet">
+                                        <a href="#"><p>Tweet</p></a>
+                                    </div>
+                                    <div class="fb">
+                                        <a href="#"><p>fb</p></a>
+                                    </div>
+                                </div>
+
+                            </div>
+
+                            <hr>
+                        </div>
+
+                        <div class="related-articles">
                     <div class="article-heading">
                         <h2><strong>Similar Articles</strong></h2>
                         <hr>
@@ -99,10 +144,78 @@
                     </div>
 
                 </div>
+                        <div class="related-articles-mob">
+                            <div class="article-heading">
+                                <h2><strong>Similar Articles</strong></h2>
+                                <hr>
+                            </div>
+                            <div class="article-item-mob" v-for="item in similar">
+                                <a v-bind:href="'/articles/'+item.slug">
+                                    <div class="row">
+                                        <div class="col-md-6 heading">
+                                            <h3>
+                                                {{item.title}}
+                                            </h3>
+                                            <div class="authors">
+                                                <ul>
+                                                    <li>{{item.updated_at}}</li>
+                                                    <li v-for="items in item.author_list"><a v-bind:href="'/author/'+items"><strong>{{items}}</strong></a></li>
+                                                </ul>
+                                            </div>
 
-            </div>
+                                        </div>
+                                        <div class="col-md-6 image">
+                                            <img v-bind:src="'https://api.guindytimes.com/'+item.image" alt="">
+                                        </div>
+                                    </div>
+                                </a>
 
-        </div>
+                                <hr>
+
+                            </div>
+
+                            <!--<div class="article-item" v-for="item in similar">-->
+                            <!--<a v-bind:href="'/articles/'+item.slug">-->
+                            <!--<div class="row">-->
+                            <!--<div class="col-md-1"></div>-->
+                            <!--<div class="col-md-3 heading">-->
+
+                            <!--<h3>-->
+                            <!--{{item.title}}-->
+                            <!--</h3>-->
+                            <!--<div class="authors">-->
+                            <!--<ul>-->
+                            <!--<li>{{item.updated_at}}</li>-->
+                            <!--<li v-for="items in item.author_list"><a v-bind:href="'/author/'+items"><strong>{{items}}</strong></a></li>-->
+                            <!--</ul>-->
+                            <!--</div>-->
+
+                            <!--</div>-->
+                            <!--<div class="col-md-4 content">-->
+
+                            <!--<p>-->
+                            <!--{{item.preview}}-->
+                            <!--</p>-->
+
+                            <!--</div>-->
+                            <!--<div class="col-md-3 image">-->
+                            <!--<img v-bind:src="'https://api.guindytimes.com/'+item.image" alt="">-->
+                            <!--</div>-->
+                            <!--<div class="col-md-1"></div>-->
+                            <!--</div>-->
+                            <!--</a>-->
+
+                            <!--<hr>-->
+
+                            <!--</div>-->
+
+                        </div>
+
+                    </div>
+                </div>
+
+
+
         <br><br><br>
         <foot></foot>
 
@@ -111,13 +224,16 @@
 </template>
 
 <script>
+    import navbar from './navbar';
     import sidebar from './sidebar';
+    import rightbar from './rightbar';
     import foot from './foot';
     import PacmanLoader from 'vue-spinner/src/PacmanLoader.vue'
 
     export default {
         name: "single_article",
         components:{
+            navbar:navbar,
             sidebar:sidebar,
             foot:foot,
             PacmanLoader
